e6c525b60e28a6222b7c833e0f24bcb853e559db
IroNinja5 <colinvandermeer@gmail.com>
2022-03-09 16:22:03 -0500

    Set to turn an angle and add comments

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,19 +14,27 @@ public class ShooterSubsystem extends SubsystemBase {
   private final CANSparkMax m_shooterMotor;
   private final CANSparkMax m_feederMotor;
   private final CANSparkMax m_winchMotor;
-  // ADD ENCODER FOR CHANGING WINCH ANGLE
+  private RelativeEncoder m_winchEncoder;
 
   private double m_shootPow = 0;
   private double m_feedPow = 0;
   private double m_winchAngle = 0;
+  private double m_winchPower = 0;
+  double winchEncoderStart = 0;
 
-  /** Creates a new ShooterSubsystem. */
+  /** Creates a new ShooterSubsystem. 
+   * @param shooterMotor Set this to the shooter motor
+   * @param feederMotor Set this to the feeder motor
+   * @param winchMotor set this to the winch motor
+  */
   public ShooterSubsystem(int shooterMotor, int feederMotor, int winchMotor) {
     m_shooterMotor = new CANSparkMax(shooterMotor, MotorType.kBrushed);
 
     m_feederMotor = new CANSparkMax(feederMotor, MotorType.kBrushless);
 
     m_winchMotor = new CANSparkMax(winchMotor, MotorType.kBrushless);
+
+    m_winchEncoder = m_winchMotor.getEncoder();
   }
 
   /**
@@ -40,15 +49,21 @@ public class ShooterSubsystem extends SubsystemBase {
     m_feedPow = feedPow;
   }
 
-  public void tunrWinch(double winchAngle) {
+  public void turnWinch(double winchAngle) {
     m_winchAngle = winchAngle;
+    m_winchPower = 0.8;
+    winchEncoderStart = m_winchEncoder.getPosition();
   }
 
   @Override
   public void periodic() {
     m_shooterMotor.set(m_shootPow);
     m_feederMotor.set(m_feedPow);
-    m_winchMotor.set(m_winchAngle);
+    m_winchMotor.set(m_winchPower);
+    if (m_winchEncoder.getPosition() - winchEncoderStart == m_winchAngle) { 
+      // Change this to divide or multiply so it is a specifc angle
+      m_winchPower = 0;
+    }
     // m_shootPow = 0;
     // m_feedPow = 0;
   }
