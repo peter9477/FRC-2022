2590f652961a092d4847d06511ad95ec18b27d7c
IroNinja5 <colinvandermeer@gmail.com>
2022-03-14 13:52:24 -0400

    Change default values to be more reasonable

--- src/main/java/frc/robot/commands/AutonomousCmdList.java
+++ src/main/java/frc/robot/commands/AutonomousCmdList.java
@@ -11,16 +11,16 @@ import frc.robot.subsystems.DrivebaseSubsystem;
 public class AutonomousCmdList extends SequentialCommandGroup {
     private ShuffleboardTab tab = Shuffleboard.getTab("Drive");
     private NetworkTableEntry autoSpeed =
-        tab.add("Autonomous Speed", 1)
+        tab.add("Autonomous Speed", 0.5)
            .getEntry();
     private NetworkTableEntry autoDistance =
-       tab.add("Autonomous Distance (inches)", 24)
+       tab.add("Autonomous Distance (inches)", 12)
           .getEntry();
            
     public AutonomousCmdList(DrivebaseSubsystem m_drivebase) {
         super();
-        double speed = autoSpeed.getDouble(1.0);
-        double distance = autoDistance.getDouble(1.0);
+        double speed = autoSpeed.getDouble(0.5);
+        double distance = autoDistance.getDouble(12.0);
         this.addCommands(new DriveDistance(m_drivebase, distance, speed));
     }
     
