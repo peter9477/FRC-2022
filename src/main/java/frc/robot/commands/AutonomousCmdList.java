468edf32395960ec8d6dd4b0c4084e3bae2206a9
IroNinja5 <colinvandermeer@gmail.com>
2022-03-14 12:14:22 -0400

    Switch autonomous to use values imported from SmartDashboard

--- src/main/java/frc/robot/commands/AutonomousCmdList.java
+++ src/main/java/frc/robot/commands/AutonomousCmdList.java
@@ -1,15 +1,27 @@
 package frc.robot.commands;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import frc.robot.subsystems.DrivebaseSubsystem;
 
 
 public class AutonomousCmdList extends SequentialCommandGroup {
-
+    private ShuffleboardTab tab = Shuffleboard.getTab("Drive");
+    private NetworkTableEntry autoSpeed =
+        tab.add("Autonomous Speed", 1)
+           .getEntry();
+    private NetworkTableEntry autoDistance =
+       tab.add("Autonomous Distance (inches)", 24)
+          .getEntry();
+           
     public AutonomousCmdList(DrivebaseSubsystem m_drivebase) {
         super();
-        this.addCommands(new DriveDistance(m_drivebase, -25, -0.5));
+        double speed = autoSpeed.getDouble(1.0);
+        double distance = autoDistance.getDouble(1.0);
+        this.addCommands(new DriveDistance(m_drivebase, distance, speed));
     }
     
     
